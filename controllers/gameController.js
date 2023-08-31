e8cb01a753806ff3374e5a83036c2aecbb016b49
Doğukan Topçu <dogukantopcu35@gmail.com>
2023-08-31 03:55:13 +0300

    level1 and level2 data connected to database

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -13,3 +13,31 @@ export const increaseScore = async (req, res) => {
         res.json(result);
     });
 };
+
+
+export const level1_addData = async (req, res) => {
+    console.log(req.body);
+    const { userId, questionImage, optionImages, userResponse, isCorrect, time } = req.body;
+    await db.query(`INSERT INTO level1 (user_id, question, options, user_response, isCorrect, time) VALUES ('${parseInt(userId)}', '${questionImage}', '${optionImages}', '${userResponse}', '${isCorrect == "false" ? 0 : 1}', '${parseInt(time)}')`, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(400).send("Error while inserting data");
+        }
+        console.log("Data inserted to Level 1 Table");
+        res.status(200).send("Data inserted");
+    });
+}
+
+
+export const level2_addData = async (req, res) => {
+    console.log(req.body);
+    const { userId, allImages, firstOption, secondOption, isCorrect, time } = req.body;
+    await db.query(`INSERT INTO level2 (user_id, all_images, first_option, second_option, isCorrect, time) VALUES ('${parseInt(userId)}', '${allImages}', '${firstOption}', '${secondOption}', '${isCorrect == "false" ? 0 : 1}', '${parseInt(time)}')`, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(400).send("Error while inserting data");
+        }
+        console.log("Data inserted to Level 2 Table");
+        res.status(200).send("Data inserted");
+    });
+}
--- routes/game.js
+++ routes/game.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { increaseScore } from "../controllers/gameController.js";
+import { increaseScore, level1_addData, level2_addData } from "../controllers/gameController.js";
 
 
 const gameRouter = express.Router();
 
 gameRouter.post("/score/increase", increaseScore);
-// gameRouter.post("/login", login);
+gameRouter.post("/level1/addData", level1_addData);
+gameRouter.post("/level2/addData", level2_addData);
 
 
 export default gameRouter;
